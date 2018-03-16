bade3ef9d0316f3df92c11b264d67c46a72ab5ce
Brendan <brendangraff@gmail.com>
2018-03-16 03:22:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,16 @@ var playlist =
   joeG = "blues a\'comin",
   ericC = "my Heart goes on"
 }
+
+function updatePlaylist(playlistA, artistName, song)
+{
+  playlistA.artistName = 'song';
+  return playlistA;
+}
+
+function removeFromPlaylist(playlistA, artistName)
+{
+  playlistA.artistName;
+  return playlistA;
+}
+
